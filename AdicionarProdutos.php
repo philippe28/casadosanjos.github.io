914d6415831a94dc2b408832e18a0dcf4a75f695
Gabriel Francisco <gabrinelson27@gmail.com>
2017-10-19 23:40:16 -0200

    Revert "Arruimei umas parada"
    This reverts commit 4a05f895dda92eafd32d2921cdf93d988103b57c.

--- AdicionarProdutos.php
+++ AdicionarProdutos.php
@@ -9,52 +9,52 @@
 </head>
 <body>
 	<div class="container">
-		<div class="row">
-			<div class="col-md-2">
-			</div>	
-			<div class="col-md-8" style="margin-top: 100px">
-				<h3 align="center">Adicionar Produto</h3>	
-				<hr>
-				<?php
-					require 'php/adminHelper.php';
-					if (!empty($_POST) && AdicionarProduto($_POST, $_FILES['imagem']['tmp_name'])){
-						echo "<div class='alert alert-success'>
-							<strong>Feito!</strong> Produto adicionado com sucesso.
-						</div>";
-					}
-					echo mysqli_error($GLOBALS['conexao']);
-				?>
-				<form action="AdicionarProdutos.php" method="post" enctype="multipart/form-data">
-					<div class="form-group">
-						<input type="text" placeholder="Nome do Produto" class="form-control" name="nome" required \>
-					</div>
-					<div class="form-group">
-						<input type="number" placeholder="Valor do Produto" class="form-control" name="valor" step=".01" required \>
-					</div>
-					<div class="form-group form-control">
-						<div class="row">
-							<div class="col-md-1">
-								<p>Imagem: </p>
-							</div>
-							<div class="col-md-11" style="left: 15px">
-								<input type="file" accept="image/*" name="imagem" required \>
-							</div>
+	<div class="row">
+		<div class="col-md-2">
+		</div>	
+		<div class="col-md-8" style="margin-top: 100px">
+			<h3 align="center">Adicionar Produto</h3>	
+			<hr>
+			<?php
+				require 'php/adminHelper.php';
+				if (!empty($_POST) && AdicionarProduto($_POST, $_FILES['imagem']['tmp_name'])){
+					echo "<div class='alert alert-success'>
+						<strong>Feito!</strong> Produto adicionado com sucesso.
+					</div>";
+				}
+				echo mysqli_error($GLOBALS['conexao']);
+			?>
+			<form action="AdicionarProdutos.php" method="post" enctype="multipart/form-data">
+				<div class="form-group">
+					<input type="text" placeholder="Nome do Produto" class="form-control" name="nome" required \>
+				</div>
+				<div class="form-group">
+					<input type="number" placeholder="Valor do Produto" class="form-control" name="valor" step=".01" required \>
+				</div>
+				<div class="form-group form-control">
+					<div class="row">
+						<div class="col-md-1">
+							<p>Imagem: </p>
+						</div>
+						<div class="col-md-11" style="left: 15px">
+							<input type="file" accept="image/*" name="imagem" required \>
 						</div>
 					</div>
-					<div class="form-group">
-						<button type="submit" class="btn btn-success btn-block">ADICIONAR</button>
-					</div>
-				</form>
-				<div class="form-group">
-					<a href="VisualizarProdutos.php">
-						<button type="submit" class="btn btn-primary btn-block">VER PRODUTOS</button>
-					</a>
 				</div>
-				<hr>
+				<div class="form-group">
+					<button type="submit" class="btn btn-success btn-block">ADICIONAR</button>
+				</div>
+			</form>
+			<div class="form-group">
+				<a href="VisualizarProdutos.php">
+					<button type="submit" class="btn btn-primary btn-block">VER PRODUTOS</button>
+				</a>
 			</div>
-			<div class="col-md-2">
-			</div>			
+			<hr>
 		</div>
+		<div class="col-md-2">
+		</div>			
+	</div>
 	</div>
 </body>
 </html>
