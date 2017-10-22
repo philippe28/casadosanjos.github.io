25f5799b2a6e4f4b45c895f85a21ba69fbd9020f
pimatco <pimatco@gmail.com>
2017-10-21 22:33:28 -0200

    carrinho mudou, melhorou, fechou

--- js/jquery.mycart.js
+++ js/jquery.mycart.js
@@ -13,7 +13,7 @@
 
     var _options = null;
     var DEFAULT_OPTIONS = {
-      currencySymbol: '$',
+      currencySymbol: 'R$',
       classCartIcon: 'my-cart-icon',
       classCartBadge: 'my-cart-badge',
       classProductQuantity: 'my-product-quantity',
